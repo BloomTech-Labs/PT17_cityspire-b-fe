734a8dbf72bab4b4a8a55c2e4c5ec619e078f6ad
angelynaliem <angelynaliem@gmail.com>
2021-03-31 22:19:50 -0400

    updated cityData action creator

--- src/state/actions/cityData.js
+++ src/state/actions/cityData.js
@@ -4,22 +4,42 @@ export const FETCHING_CITY_START = 'FETCHING_CITY_START';
 export const FETCHING_CITY_SUCCESS = 'FETCHING_CITY_SUCCESS';
 export const FETCHING_CITY_ERROR = 'FETCHING_CITY_ERROR';
 
-export const fetchCityData = cityInfo => {
-  return async dispatch => {
-    dispatch({ type: FETCHING_CITY_START });
+export const fetchCityData = (cityInfo, history) => dispatch => {
+  dispatch({ type: FETCHING_CITY_START });
 
-    try {
-      const res = await axios.post(
-        `https://localhost:8000/api/get_data`,
-        cityInfo
-      );
+  axios()
+    .post('https://localhost:8000/api/get_data', cityInfo)
+
+    .then(res => {
+      console.log('Action creater fetchCityData successful: ', res.data);
 
       dispatch({
         type: FETCHING_CITY_SUCCESS,
         payload: res.data,
       });
-    } catch (err) {
-      dispatch({ type: FETCHING_CITY_ERROR, payload: err.detail });
-    }
-  };
+
+      history.push('/${res.data.state}/${res.data.city}');
+    })
+
+    .catch(err => {
+      console.log('Action creator fetchCityData failed', err.message);
+    });
 };
+
+// return async dispatch => {
+//   dispatch({ type: FETCHING_CITY_START });
+
+//   try {
+//     const res = await axios.post(
+//       `https://localhost:8000/api/get_data`,
+//       cityInfo
+//     );
+
+//     dispatch({
+//       type: FETCHING_CITY_SUCCESS,
+//       payload: res.data,
+//     });
+//   } catch (err) {
+//     dispatch({ type: FETCHING_CITY_ERROR, payload: err.detail });
+//   }
+// };
